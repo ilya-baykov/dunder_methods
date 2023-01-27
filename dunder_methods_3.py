1b6335873046e82ebcee71ab55a3f5f1275ab651
ilya-baykov <i-baykov@internet.ru>
2023-01-28 00:53:00 +0300

    Создал класс - Декоратор

--- dunder_methods_3.py
+++ dunder_methods_3.py
@@ -1,10 +1,32 @@
+import time
+
+
 class Addition:
     def __call__(self, *args, **kwargs):
         print(f"Сумма переданных значений = {sum([arg for arg in args if isinstance(arg, (int, float))])}")
 
 
+class Timer:
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        start_time = time.time()
+        self.func()
+        finish_time = time.time()
+        print(f"Время выполнения Функции {self.func.__name__} составило {finish_time - start_time}c")
+
+
+@Timer
+def calculate():
+    for i in range(10_000_000):
+        2 ** 100
+
+
+@Timer
+def calculate_2():
+    [i for i in range(10_000_000)]
+
+
 if __name__ == "__main__":
-    add = Addition()
-    add(10, 20)
-    add(1, 2, 3.4)
-    add(1, 2, "hELLO", [1, 2], 3)
+    pass
