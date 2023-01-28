0841ae49efdc856579c4132dbde3dee25d8fc397
ilya-baykov <i-baykov@internet.ru>
2023-01-29 00:35:22 +0300

    Добавил класс My_List, который имеет возможность "разряженного массива"

--- getitem.py
+++ getitem.py
@@ -25,3 +25,23 @@ class Data:
             del self.data[key]
 
 
+class My_List:
+    def __init__(self, *args):
+        self.values = list(args)
+
+    def __getitem__(self, item):
+        if 0 < item < len(self.values):
+            return self.values[item]
+        else:
+            raise ValueError("Такого индекса не существует")
+
+    def __setitem__(self, key, value):
+        if 0 < key < len(self.values):
+            self.values[key] = value
+        elif key > len(self.values):
+            df = key - len(self.values) + 1
+            self.values.extend([None] * df)
+            self.values[key] = value
+
+    def __str__(self):
+        return f"{self.values}"
